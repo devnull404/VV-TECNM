1434412ca9f1a92ae8afd5b9a72a0b252663b976
Francisco <ecoli404@icloud.com>
2020-11-19 15:52:04 -0600

    Pruebas

--- work-01/main.py
+++ work-01/main.py
@@ -5,4 +5,12 @@ print("Se han cargado las librerías correctamente")
 
 I1, I2, I3 = [plt.imread("./img/3.jpg"), plt.imread("./img/2.jpg"), plt.imread("./img/3.jpg")]
 
-I1_r = np.reshape(I1, (3,-1))
+n, m, c = I1.shape
+
+I1_mu = np.mean(I1[:][:][0])
+
+for i in range(n):
+    for j in range(m):
+        print(I1[i][j][0])
+
+print(I1_mu)
